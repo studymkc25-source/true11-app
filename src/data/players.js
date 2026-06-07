440cdad5736d442e2b732292eef07cd8d78bd8a1
studymk25-source <studymkc25@gmail.com>
2026-06-07 11:55:29 +0530

    Added real player photos

--- src/data/players.js
+++ src/data/players.js
@@ -1,19 +1,90 @@
-export const PLAYERS_DATA = [
-  // MATCH 1 - IND vs AUS
-  { id: 1, matchId: 1, name: "Rohit Sharma", team: "IND", role: "BAT", credits: 10.5, img: "https://img1.hscicdn.com/image/upload/f_auto,t_ds_square_w_160,q_50/lsci/db/PICTURES/CMS/316600/316605.png", selectedBy: 85.2 },
-  { id: 2, matchId: 1, name: "Virat Kohli", team: "IND", role: "BAT", credits: 10.0, img: "https://img1.hscicdn.com/image/upload/f_auto,t_ds_square_w_160,q_50/lsci/db/PICTURES/CMS/316600/316605.png", selectedBy: 92.1 },
-  { id: 3, matchId: 1, name: "KL Rahul", team: "IND", role: "WK", credits: 9.0, img: "https://img1.hscicdn.com/image/upload/f_auto,t_ds_square_w_160,q_50/lsci/db/PICTURES/CMS/316600/316605.png", selectedBy: 75.3 },
-  { id: 4, matchId: 1, name: "Hardik Pandya", team: "IND", role: "AR", credits: 9.5, img: "https://img1.hscicdn.com/image/upload/f_auto,t_ds_square_w_160,q_50/lsci/db/PICTURES/CMS/316600/316605.png", selectedBy: 88.7 },
-  { id: 5, matchId: 1, name: "Jasprit Bumrah", team: "IND", role: "BWL", credits: 9.0, img: "https://img1.hscicdn.com/image/upload/f_auto,t_ds_square_w_160,q_50/lsci/db/PICTURES/CMS/316600/316605.png", selectedBy: 82.4 },
-  
-  { id: 6, matchId: 1, name: "David Warner", team: "AUS", role: "BAT", credits: 9.5, img: "https://img1.hscicdn.com/image/upload/f_auto,t_ds_square_w_160,q_50/lsci/db/PICTURES/CMS/316600/316605.png", selectedBy: 78.9 },
-  { id: 7, matchId: 1, name: "Steve Smith", team: "AUS", role: "BAT", credits: 9.0, img: "https://img1.hscicdn.com/image/upload/f_auto,t_ds_square_w_160,q_50/lsci/db/PICTURES/CMS/316600/316605.png", selectedBy: 76.2 },
-  { id: 8, matchId: 1, name: "Glenn Maxwell", team: "AUS", role: "AR", credits: 9.0, img: "https://img1.hscicdn.com/image/upload/f_auto,t_ds_square_w_160,q_50/lsci/db/PICTURES/CMS/316600/316605.png", selectedBy: 81.5 },
-  { id: 9, matchId: 1, name: "Pat Cummins", team: "AUS", role: "BWL", credits: 8.5, img: "https://img1.hscicdn.com/image/upload/f_auto,t_ds_square_w_160,q_50/lsci/db/PICTURES/CMS/316600/316605.png", selectedBy: 79.8 },
-  { id: 10, matchId: 1, name: "Mitchell Starc", team: "AUS", role: "BWL", credits: 8.5, img: "https://img1.hscicdn.com/image/upload/f_auto,t_ds_square_w_160,q_50/lsci/db/PICTURES/CMS/316600/316605.png", selectedBy: 77.3 },
-  { id: 11, matchId: 1, name: "Alex Carey", team: "AUS", role: "WK", credits: 8.0, img: "https://img1.hscicdn.com/image/upload/f_auto,t_ds_square_w_160,q_50/lsci/db/PICTURES/CMS/316600/316605.png", selectedBy: 65.1 },
-
-  // MATCH 2 - CSK vs MI
-  { id: 12, matchId: 2, name: "MS Dhoni", team: "CSK", role: "WK", credits: 8.5, img: "https://img1.hscicdn.com/image/upload/f_auto,t_ds_square_w_160,q_50/lsci/db/PICTURES/CMS/316600/316605.png", selectedBy: 95.5 },
-  { id: 13, matchId: 2, name: "Ruturaj Gaikwad", team: "CSK", role: "BAT", credits: 9.0, img: "https://img1.hscicdn.com/image/upload/f_auto,t_ds_square_w_160,q_50/lsci/db/PICTURES/CMS/316600/316605.png", selectedBy: 84.2 },
+export const players = [
+  {
+    id: 1,
+    name: "Virat Kohli",
+    team: "IND",
+    role: "BAT",
+    credits: 10.5,
+    image: "https://img1.hscicdn.com/image/upload/f_auto,t_ds_square_w_320/lsci/db/PICTURES/CMS/316600/316605.png"
+  },
+  {
+    id: 2,
+    name: "Rohit Sharma", 
+    team: "IND",
+    role: "BAT",
+    credits: 10,
+    image: "https://img1.hscicdn.com/image/upload/f_auto,t_ds_square_w_320/lsci/db/PICTURES/CMS/316500/316584.png"
+  },
+  {
+    id: 3,
+    name: "Jasprit Bumrah",
+    team: "IND", 
+    role: "BOWL",
+    credits: 9.5,
+    image: "https://img1.hscicdn.com/image/upload/f_auto,t_ds_square_w_320/lsci/db/PICTURES/CMS/316500/316596.png"
+  },
+  {
+    id: 4,
+    name: "MS Dhoni",
+    team: "CSK",
+    role: "WK", 
+    credits: 9,
+    image: "https://img1.hscicdn.com/image/upload/f_auto,t_ds_square_w_320/lsci/db/PICTURES/CMS/316500/316598.png"
+  },
+  {
+    id: 5,
+    name: "KL Rahul",
+    team: "IND",
+    role: "WK",
+    credits: 9.5,
+    image: "https://img1.hscicdn.com/image/upload/f_auto,t_ds_square_w_320/lsci/db/PICTURES/CMS/316500/316599.png"
+  },
+  {
+    id: 6,
+    name: "Hardik Pandya",
+    team: "IND",
+    role: "AR",
+    credits: 9,
+    image: "https://img1.hscicdn.com/image/upload/f_auto,t_ds_square_w_320/lsci/db/PICTURES/CMS/316500/316593.png"
+  },
+  {
+    id: 7,
+    name: "Ravindra Jadeja",
+    team: "IND",
+    role: "AR", 
+    credits: 9,
+    image: "https://img1.hscicdn.com/image/upload/f_auto,t_ds_square_w_320/lsci/db/PICTURES/CMS/316500/316594.png"
+  },
+  {
+    id: 8,
+    name: "Mohammed Shami",
+    team: "IND",
+    role: "BOWL",
+    credits: 8.5,
+    image: "https://img1.hscicdn.com/image/upload/f_auto,t_ds_square_w_320/lsci/db/PICTURES/CMS/316500/316592.png"
+  },
+  {
+    id: 9,
+    name: "Shubman Gill",
+    team: "IND",
+    role: "BAT",
+    credits: 9,
+    image: "https://img1.hscicdn.com/image/upload/f_auto,t_ds_square_w_320/lsci/db/PICTURES/CMS/356800/356813.png"
+  },
+  {
+    id: 10,
+    name: "Suryakumar Yadav",
+    team: "IND", 
+    role: "BAT",
+    credits: 9,
+    image: "https://img1.hscicdn.com/image/upload/f_auto,t_ds_square_w_320/lsci/db/PICTURES/CMS/325900/325974.png"
+  },
+  {
+    id: 11,
+    name: "Kuldeep Yadav",
+    team: "IND",
+    role: "BOWL",
+    credits: 8.5,
+    image: "https://img1.hscicdn.com/image/upload/f_auto,t_ds_square_w_320/lsci/db/PICTURES/CMS/316500/316597.png"
+  }
 ];
